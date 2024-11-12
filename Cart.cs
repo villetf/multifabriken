7da3446150b603a99636baebbe49c501e7d0d23f
Vilhelm Fontell <ville@fontell.se>
2024-11-12 16:25:04 +0100

    Fixa återstående fel (issue #21)

--- Cart.cs
+++ Cart.cs
@@ -1,5 +1,4 @@
 using System.Text.RegularExpressions;
-using System;
 
 namespace multifabriken;
 
@@ -12,6 +11,7 @@ public class Cart
    }
 
    public void AddCar() {
+      Console.Clear();
       Console.WriteLine("Vilket märke vill du ha?");
       string? brand = Console.ReadLine();
       Console.WriteLine("Vilken färg vill du ha?");
@@ -33,32 +33,109 @@ public class Cart
       }
       Car NewCar = new Car(licensePlate!, color!, brand!);
       this.Products.Add(NewCar);
+      Console.WriteLine("Produkten har lagts till i varukorgen.");
+      Thread.Sleep(2000);
    }
 
    public void AddCandy() {
+      Console.Clear();
       Console.WriteLine("Vilken smak vill du ha?");
       string? flavour = Console.ReadLine();
-      Console.WriteLine("Hur många vill du ha?");
-      int number = int.Parse(Console.ReadLine());
-      Candy NewCandy = new Candy(flavour!, number);
+
+      bool continueRepeat = true;
+      int amount = 0;
+      while (continueRepeat)
+      {
+         Console.WriteLine("Hur många vill du ha?");
+         string? amountString = Console.ReadLine();
+         if (!int.TryParse(amountString, out amount))
+         {
+            Console.WriteLine("Du måste ange ett heltal.");
+         } else
+         {
+            continueRepeat = false;
+         }
+      }
+
+      Candy NewCandy = new Candy(flavour!, amount);
       this.Products.Add(NewCandy);
+      Console.WriteLine("Produkten har lagts till i varukorgen.");
+      Thread.Sleep(2000);
    }
 
    public void AddPipe() {
-      Console.WriteLine("Vilken diameter vill du ha på röret?");
-      float diameter = float.Parse(Console.ReadLine());
-      Console.WriteLine("Hur långt rör vill du ha?");
-      float length = float.Parse(Console.ReadLine());
+      Console.Clear();
+      bool continueRepeat = true;
+      float diameter = 0;
+      while (continueRepeat)
+      {
+         Console.WriteLine("Vilken diameter vill du ha på röret (i meter)?");
+         string? diameterString = Console.ReadLine();
+         if (!float.TryParse(diameterString, out diameter))
+         {
+            Console.WriteLine("Du måste ange ett heltal eller decimaltal.");
+         } else
+         {
+            continueRepeat = false;
+         }
+      }
+
+      continueRepeat = true;
+      float length = 0;
+      while (continueRepeat)
+      {
+         Console.WriteLine("Hur långt rör vill du ha (i meter)?");
+         string? lengthString = Console.ReadLine();
+         if (!float.TryParse(lengthString, out length))
+         {
+            Console.WriteLine("Du måste ange ett heltal eller decimaltal.");
+         } else
+         {
+            continueRepeat = false;
+         }
+      }
+
       Pipe NewPipe = new Pipe(length, diameter);
       this.Products.Add(NewPipe);
+      Console.WriteLine("Produkten har lagts till i varukorgen.");
+      Thread.Sleep(2000);
    }
 
    public void AddOatmilk() {
-      Console.WriteLine("Hur många liter vill du ha?");
-      float amount = float.Parse(Console.ReadLine());
-      Console.WriteLine("Vilken fetthalt vill du ha?");
-      float fat = float.Parse(Console.ReadLine());
+      Console.Clear();
+      bool continueRepeat = true;
+      float amount = 0;
+      while (continueRepeat)
+      {
+         Console.WriteLine("Hur många liter vill du ha?");
+         string? amountString = Console.ReadLine();
+         if (!float.TryParse(amountString, out amount))
+         {
+            Console.WriteLine("Du måste ange ett heltal eller decimaltal.");
+         } else
+         {
+            continueRepeat = false;
+         }
+      }
+
+      continueRepeat = true;
+      float fat = 0;
+      while (continueRepeat)
+      {
+         Console.WriteLine("Vilken fetthalt vill du ha (i procent)?");
+         string? fatString = Console.ReadLine();
+         if (!float.TryParse(fatString, out fat))
+         {
+            Console.WriteLine("Du måste ange ett heltal eller decimaltal.");
+         } else
+         {
+            continueRepeat = false;
+         }
+      }
+
       Oatmilk NewOatMilk = new Oatmilk(fat, amount);
       this.Products.Add(NewOatMilk);
+      Console.WriteLine("Produkten har lagts till i varukorgen.");
+      Thread.Sleep(2000);
    }
 }
