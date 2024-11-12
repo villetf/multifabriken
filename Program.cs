4ef4f875c71d66c1f8209ecc3672eee7890c944e
Vilhelm Fontell <ville@fontell.se>
2024-11-12 15:21:14 +0100

    Skapa metod för att lägga till objekt i kundkorgen (issue #8)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿
 using multifabriken;
 
 bool continueRepeat = true;
@@ -28,51 +28,16 @@ while (continueRepeat)
    switch (answer)
    {
       case 1:
-         Console.WriteLine("Vilket märke vill du ha?");
-         string? brand = Console.ReadLine();
-         Console.WriteLine("Vilken färg vill du ha?");
-         string? color = Console.ReadLine();
-         bool continueLicenseLoop = true;
-         string? licensePlate = "";
-         while (continueLicenseLoop)
-         {
-            Console.WriteLine("Vilket registreringsnummer vill du ha?");
-            licensePlate = Console.ReadLine();
-            if (!Regex.IsMatch(licensePlate!, @"^\w{3}(\d{3}|\d{2}\w)$"))
-            {
-               Console.WriteLine("Du måste ange ett registreringsnummer i formatet ABC123 alternativt ABC12X.");
-            } else
-            {
-               licensePlate = licensePlate!.ToUpper();
-               continueLicenseLoop = false;
-            }
-         }
-         Car NewCar = new Car(licensePlate!, color!, brand!);
-         Cart.Products.Add(NewCar);
+         Cart.AddCar();
          break;
       case 2:
-         Console.WriteLine("Vilken smak vill du ha?");
-         string? flavour = Console.ReadLine();
-         Console.WriteLine("Hur många vill du ha?");
-         int number = int.Parse(Console.ReadLine());
-         Candy NewCandy = new Candy(flavour!, number);
-         Cart.Products.Add(NewCandy);
+         Cart.AddCandy();
          break;
       case 3:
-         Console.WriteLine("Vilken diameter vill du ha på röret?");
-         float diameter = float.Parse(Console.ReadLine());
-         Console.WriteLine("Hur långt rör vill du ha?");
-         float length = float.Parse(Console.ReadLine());
-         Pipe NewPipe = new Pipe(length, diameter);
-         Cart.Products.Add(NewPipe);
+         Cart.AddPipe();
          break;
       case 4:
-         Console.WriteLine("Hur många liter vill du ha?");
-         float amount = float.Parse(Console.ReadLine());
-         Console.WriteLine("Vilken fetthalt vill du ha?");
-         float fat = float.Parse(Console.ReadLine());
-         Oatmilk NewOatMilk = new Oatmilk(fat, amount);
-         Cart.Products.Add(NewOatMilk);
+         Cart.AddOatmilk();
          break;
       case 5:
          Console.WriteLine("Din kundkorg innehåller:");
